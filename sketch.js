999f223c1af1a85769b7bf6c4a477f3f7b073e7c
SolidSnake2048 <88735858+SolidSnake2048@users.noreply.github.com>
2021-09-02 11:03:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload()
 {
 	starImg = loadImage("images/star.png");
 	bgImg = loadImage("images/starNight.png");
-	fairyImg=loadAnimation("images/fairyimage1.png","images/fairyimage2.png");
+	fairyImg=loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	fairyVoice=loadSound("sound/JoyMusic.mp3");
 	//carga aquí la animación del hada
 }
